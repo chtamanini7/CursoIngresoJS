6acbb9edacab5382171fac56f0038a9b34d889ab
chtamanini7 <66229115+chtamanini7@users.noreply.github.com>
2021-07-12 17:18:46 -0300

    Add files via upload

---
+++ 1-EntradaSalida/1-EntradaSalida/jsEntradaSalida-10.js
@@ -0,0 +1,10 @@
+/*
+Debemos lograr tomar el importe por ID.
+Transformarlo a entero (parseInt), luego
+mostrar el importe con un Descuento del 25 %
+en el cuadro de texto "RESULTADO"*/
+function mostrarAumento()
+{
+	var sueldo = txtIdImporte.value;
+	txtIdResultado.value = sueldo * 0.75;
+}
